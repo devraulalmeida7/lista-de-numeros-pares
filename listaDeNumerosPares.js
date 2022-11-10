71f0067c3aa2f3974d658761aa67653e43f5c587
Raul Almeida <77411564+devraulalmeida7@users.noreply.github.com>
2022-11-10 20:49:45 -0300

    Update
    Transformei o código para ser mais genérico.

--- listaDeNumerosPares.js
+++ listaDeNumerosPares.js
@@ -1,9 +1,9 @@
-lista = [0,2,10,19,21,25,30,9,47,97]
+var quantidadeNumeros = parseInt(prompt("Digite a quantidade de números"));
 
 p = 0;
-
-for(let i = 0; i< lista.length; i++) {
-    if (lista[i] % 2 ==0) {
+var numero = parseInt(prompt("Digite o número desejado"));
+for(let i = 0; i< quantidadeNumeros; i++) {
+    if (numero % 2 ==0) {
         p++
         console.log("O número é par")
     }
